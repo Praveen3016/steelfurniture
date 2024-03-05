09f4ce004819c6c95fd0a6b40bf2c19d9c5488bf
tilak9413 <tilak9740@gmail.com>
2024-03-05 15:21:41 +0530

    fffffff

--- dineshbhai/src/component/clientnumber.jsx
+++ dineshbhai/src/component/clientnumber.jsx
@@ -5,7 +5,8 @@ function Clientnumber() {
 return (
 
 <>
-<img style={{height:"330px", width:"100%"}}  className='img-fluid ' src="https://th.bing.com/th/id/OIP.YNbaMdY3hI3A8f4ck_6B3gHaFW?rs=1&pid=ImgDetMain" alt="" />
+<img style={{height:"330px", width:"100%"}}  className='img-fluid ' src="https://th.bing.com/th/id/OIP.YNbaMdY3hI3A8f4ck_6B3gHaFW?rs=1&pid=ImgDetMain" alt=""/>
+
 
 
 
